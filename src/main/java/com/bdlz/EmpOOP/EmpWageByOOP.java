7a39b658af1d68e501f9eacd3e7bf9f430ac1ecc
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-09-04 15:58:00 +0530

    Added: Emp Present Or Absent

--- src/main/java/com/bdlz/EmpOOP/EmpWageByOOP.java
+++ src/main/java/com/bdlz/EmpOOP/EmpWageByOOP.java
@@ -2,6 +2,15 @@ package com.bdlz.EmpOOP;
 
 public class EmpWageByOOP {
     public static void main(String[] args) {
-        System.out.println("Welcome to EmpWage program by OOPs");
+        empPresentOrAbsent();
+    }
+    public static void empPresentOrAbsent() {
+        int IS_FULL_TIME = 1;
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if (empCheck == IS_FULL_TIME) {
+            System.out.println("employee is Present");
+        } else {
+            System.out.println("employee is Absent");
+        }
     }
 }
