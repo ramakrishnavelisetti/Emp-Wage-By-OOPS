2c19869f40dbbee8097243dfb3bdc431a8753191
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-09-04 21:15:39 +0530

    Added: EmpWage For Month And Hours

--- src/main/java/com/bdlz/EmpOOP/EmpWageByOOP.java
+++ src/main/java/com/bdlz/EmpOOP/EmpWageByOOP.java
@@ -5,13 +5,16 @@ public class EmpWageByOOP {
     public static final int IS_PART_TIME = 2;
     public static final int EMP_RATE_PER_HOUR = 20;
     public static final int NO_OF_WORKING_DAYS = 20;
+    public static final int MAX_HRS_PER_MONTH = 100;
     public static  double empCheck = Math.floor(Math.random() * 10) % 3;
     public static int empHrs = 0;
     public static int empWage = 0;
     public static int totalEmpWage = 0;
+    public static int totalHrs = 0;
+    public static int day = 1;
 
-    public static void empWagePerMonth() {
-        for (int day = 1; day<=NO_OF_WORKING_DAYS; day++) {
+    public static void empWagePerMonthAndHour() {
+        while (day <= NO_OF_WORKING_DAYS && totalHrs < MAX_HRS_PER_MONTH){
             double empCheck = Math.floor(Math.random() * 10) % 3;
             switch ((int) empCheck) {
                 case IS_FULL_TIME:
@@ -25,11 +28,16 @@ public class EmpWageByOOP {
             }
             empWage = empHrs * EMP_RATE_PER_HOUR;
             totalEmpWage += empWage;
+            totalHrs += empHrs;
+            System.out.println("day " + day);
             System.out.println("Emp Wage: " + empWage);
+            day++;
         }
-        System.out.println("Total Emp Wage For Month: " + totalEmpWage);
+
+        System.out.println("Total hours: " + totalHrs);
+        System.out.println("Total Emp Wage: " + totalEmpWage);
     }
     public static void main(String[] args) {
-        empWagePerMonth();
+        empWagePerMonthAndHour();
     }
 }
