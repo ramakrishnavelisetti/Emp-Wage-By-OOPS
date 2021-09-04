69ccae27ea171766614704b681a5b0ce58348d96
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-09-04 16:19:24 +0530

    Added: Emp Daily Wage

--- src/main/java/com/bdlz/EmpOOP/EmpWageByOOP.java
+++ src/main/java/com/bdlz/EmpOOP/EmpWageByOOP.java
@@ -1,16 +1,22 @@
 package com.bdlz.EmpOOP;
 
 public class EmpWageByOOP {
-    public static void main(String[] args) {
-        empPresentOrAbsent();
-    }
-    public static void empPresentOrAbsent() {
-        int IS_FULL_TIME = 1;
-        double empCheck = Math.floor(Math.random() * 10) % 2;
-        if (empCheck == IS_FULL_TIME) {
-            System.out.println("employee is Present");
-        } else {
-            System.out.println("employee is Absent");
+    public static final int IS_FULL_TIME = 1;
+    public static final int EMP_RATE_PER_HOUR = 20;
+    public static  double empCheck = Math.floor(Math.random() * 10) % 2;
+    public static int empHrs = 0;
+    public static int empWage = 0;
+
+    public static void empDailyWage() {
+        if ( empCheck == IS_FULL_TIME) {
+            empHrs = 8;
+        }else {
+            empHrs = 0;
         }
+        empWage = empHrs * EMP_RATE_PER_HOUR;
+        System.out.println("Emp Wage:" + empWage);
+    }
+    public static void main(String[] args) {
+        empDailyWage();
     }
 }
