5897fee2e416e23680626ab1ab0f5d6a6e7774d2
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-09-07 08:19:14 +0530

    EmpWage Multi Company

--- src/main/java/com/bdlz/EmpOOP/EmpWageByOOP.java
+++ src/main/java/com/bdlz/EmpOOP/EmpWageByOOP.java
@@ -3,41 +3,38 @@ package com.bdlz.EmpOOP;
 public class EmpWageByOOP {
     public static final int IS_FULL_TIME = 1;
     public static final int IS_PART_TIME = 2;
-    public static final int EMP_RATE_PER_HOUR = 20;
-    public static final int NO_OF_WORKING_DAYS = 20;
-    public static final int MAX_HRS_PER_MONTH = 100;
     public static  double empCheck = Math.floor(Math.random() * 10) % 3;
-    public static int empHrs = 0;
-    public static int empWage = 0;
-    public static int totalEmpWage = 0;
-    public static int totalHrs = 0;
-    public static int day = 1;
-
-    public static void empWagePerMonthAndHour() {
-        while (day <= NO_OF_WORKING_DAYS && totalHrs < MAX_HRS_PER_MONTH){
-            double empCheck = Math.floor(Math.random() * 10) % 3;
-            switch ((int) empCheck) {
-                case IS_FULL_TIME:
-                    empHrs = 8;
-                    break;
+    public int multiCompany(String company, int empRatePerHr, int numWorkingDays, int maxHrs) {
+        int empHrs = 0, totalEmpHrs = 8, totalWorkingDays = 0;
+        while (totalEmpHrs < maxHrs && totalWorkingDays < numWorkingDays) {
+            totalWorkingDays++;
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+            switch (empCheck) {
                 case IS_PART_TIME:
                     empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
                     break;
                 default:
                     empHrs = 0;
             }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            totalEmpWage += empWage;
-            totalHrs += empHrs;
-            System.out.println("day " + day);
-            System.out.println("Emp Wage: " + empWage);
-            day++;
+            totalEmpHrs += empHrs;
         }
-
-        System.out.println("Total hours: " + totalHrs);
-        System.out.println("Total Emp Wage: " + totalEmpWage);
+        int totalEmpWage = totalEmpHrs * empRatePerHr;
+        System.out.println("Company Name : " + company );
+        System.out.println("Emp Rate perHr : " + empRatePerHr);
+        System.out.println("Number Of Working Days : " + numWorkingDays);
+        System.out.println("Max Hours : " + maxHrs);
+        System.out.println("Total EmpWage : " + totalEmpWage);
+        return totalEmpWage;
     }
+
     public static void main(String[] args) {
-        empWagePerMonthAndHour();
+        System.out.println("Welcome to EmployeeWage builder");
+        EmpWageByOOP dMart= new EmpWageByOOP();
+        dMart.multiCompany("Dmart",25,22,100);
+        EmpWageByOOP relianceDigital= new EmpWageByOOP();
+        relianceDigital.multiCompany("Reliance Digital",40,25,200);
     }
 }
