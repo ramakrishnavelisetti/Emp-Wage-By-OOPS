388586beb41e14db30609ef87d2303145d384576
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-09-07 20:17:37 +0530

    Save TotalWage Each Comp

--- src/main/java/com/bdlz/EmpOOP/EmpWageByOOP.java
+++ src/main/java/com/bdlz/EmpOOP/EmpWageByOOP.java
@@ -3,10 +3,23 @@ package com.bdlz.EmpOOP;
 public class EmpWageByOOP {
     public static final int IS_FULL_TIME = 1;
     public static final int IS_PART_TIME = 2;
-    public static  double empCheck = Math.floor(Math.random() * 10) % 3;
-    public int multiCompany(String company, int empRatePerHr, int numWorkingDays, int maxHrs) {
+
+    private final String company;
+    private final int empRatePerHr;
+    private final int numWorkingDays;
+    private final int maxHoursPerMonth;
+    private int totalEmpWage;
+
+    public EmpWageByOOP(String company, int empRatePerHr, int numWorkingDays, int maxHoursPerMonth) {
+        this.company = company;
+        this.empRatePerHr = empRatePerHr;
+        this.numWorkingDays = numWorkingDays;
+        this.maxHoursPerMonth = maxHoursPerMonth;
+    }
+
+    public int multiCompany() {
         int empHrs = 0, totalEmpHrs = 8, totalWorkingDays = 0;
-        while (totalEmpHrs < maxHrs && totalWorkingDays < numWorkingDays) {
+        while (totalEmpHrs < maxHoursPerMonth && totalWorkingDays < numWorkingDays) {
             totalWorkingDays++;
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
             switch (empCheck) {
@@ -20,21 +33,22 @@ public class EmpWageByOOP {
                     empHrs = 0;
             }
             totalEmpHrs += empHrs;
+            System.out.println("Day:" + totalWorkingDays + " Emp Hr:" + empHrs);
         }
-        int totalEmpWage = totalEmpHrs * empRatePerHr;
-        System.out.println("Company Name : " + company );
-        System.out.println("Emp Rate perHr : " + empRatePerHr);
-        System.out.println("Number Of Working Days : " + numWorkingDays);
-        System.out.println("Max Hours : " + maxHrs);
-        System.out.println("Total EmpWage : " + totalEmpWage);
-        return totalEmpWage;
+        totalEmpWage = totalEmpHrs * empRatePerHr;
+        return empHrs;
     }
 
+    public String toString() {
+        return ("Total emp Wage for company :" + company + " is: " + totalEmpWage);
+    }
     public static void main(String[] args) {
-        System.out.println("Welcome to EmployeeWage builder");
-        EmpWageByOOP dMart= new EmpWageByOOP();
-        dMart.multiCompany("Dmart",25,22,100);
-        EmpWageByOOP relianceDigital= new EmpWageByOOP();
-        relianceDigital.multiCompany("Reliance Digital",40,25,200);
+        System.out.println(" Welcome to EmployeeWage builder ");
+        EmpWageByOOP dMart = new EmpWageByOOP("Dmart", 20, 20, 100);
+        EmpWageByOOP reliance = new EmpWageByOOP("Reliance", 10, 20, 110);
+        dMart.multiCompany();
+        System.out.println(dMart);
+        reliance.multiCompany();
+        System.out.println(reliance);
     }
 }
